2761be7476ae9791c92b5e64a8bd3f61bd5c8c2c
livingstone23 <livingstone23@gmail.com>
2023-06-29 21:16:50 +0200

    Initial Charge

---
+++ src/pages/_app.js
@@ -0,0 +1,7 @@
+import 'semantic-ui-css/semantic.min.css'
+import "@/scss/global.scss";
+
+export default function App(props) {
+  const { Component, pageProps } = props;
+  return <Component {...pageProps} />
+}
---
+++ src/utils/constants.js
@@ -0,0 +1,16 @@
+export const ENV = {
+    API_URL:"generada_por_amazon",
+    MEDIA_API: "https://boz25twvl6.execute-api.us-east-1.amazonaws.com/Produccion/gambit-lcano",
+    MEDIA_URL: "https://gambit-lcano.s3.amazonaws.com",
+    ENDPOINTS: {
+        USERS:"users",
+        USER_ME:"user/me",
+        ADDRESS:"address",
+        CATEGORY:"category",
+        PRODUCT:"product",
+        ORDER:"order",
+        ORDERS:"orders",
+    },
+    TOKEN:"token",
+    BASKET:"basket"
+};
